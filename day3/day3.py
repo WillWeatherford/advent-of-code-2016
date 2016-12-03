0e65fba0560c297b11387450c5883cf19913a05e
Will Weatherford <weatherford.william@gmail.com>
2016-12-03 00:17:44 -0800

    Solved Part 2.

--- day3/day3.py
+++ day3/day3.py
@@ -10,6 +10,22 @@ The design document gives the side lengths of each triangle it describes, but...
 In a valid triangle, the sum of any two sides must be larger than or equal to the remaining side. For example, the "triangle" given above is impossible, because 5 + 10 is not larger than 25.
 
 In your puzzle input, how many of the listed triangles are possible?
+
+--- Part Two ---
+
+Now that you've helpfully marked up their design documents, it occurs to you that triangles are specified in groups of three vertically. Each set of three numbers in a column specifies a triangle. Rows are unrelated.
+
+For example, given the following specification, numbers with the same hundreds digit would be part of the same triangle:
+
+101 301 501
+102 302 502
+103 303 503
+201 401 601
+202 402 602
+203 403 603
+
+In your puzzle input, and instead reading by columns, how many of the listed triangles are possible?
+
 """
 
 INPUT_FILE = 'day3_input.txt'
@@ -22,18 +38,41 @@ def input_lines(filename):
             yield line.strip()
 
 
-def count_valid_triangles(lines):
+def is_valid_triangle(numbers):
+    """Return boolean of whether a set of integers is a valid triangle."""
+    numbers = sorted(numbers)
+    return sum(numbers[:2]) > numbers[-1]
+
+
+def count_valid_triangles_by_line(lines):
     """Return an integer of the number of valid triangles found in input."""
     count = 0
     for line in lines:
-        numbers = set(map(int, line.split()))
-        largest = max(numbers)
-        numbers.remove(largest)
-        count += sum(numbers) >= largest
+        numbers = map(int, line.split())
+        count += is_valid_triangle(numbers)
+    return count
+
+
+def count_valid_triangles_by_column(lines):
+    """Return an integer of the number of valid triangles found in input."""
+    count = 0
+    while True:
+        try:
+            three_lines = [map(int, next(lines).split()) for _ in range(3)]
+        except StopIteration:
+            break
+        rotated = zip(*reversed(three_lines))
+        count += sum(map(is_valid_triangle, rotated))
     return count
 
 
 if __name__ == '__main__':
     lines = input_lines(INPUT_FILE)
-    result = count_valid_triangles(lines)
-    print('There are {} valid triangles.'.format(result))
+    result1 = count_valid_triangles_by_line(lines)
+    print('There are {} valid triangles, looking at each line as a triangle.'
+          ''.format(result1))
+
+    lines = input_lines(INPUT_FILE)
+    result2 = count_valid_triangles_by_column(lines)
+    print('There are {} valid triangles, looking at 3 numbers in each column '
+          'as a triangle.'.format(result2))
