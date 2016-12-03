51a4b7c00ddef24b5751903f5886e51280255a4a
Will Weatherford <weatherford.william@gmail.com>
2016-12-03 00:54:15 -0800

    Refactored to include both solutions in one file.

--- day1/day1_1.py
+++ day1/day1_1.py
@@ -17,11 +17,18 @@ Following R2, L3 leaves you 2 blocks East and 3 blocks North, or 5 blocks away.
 R2, R2, R2 leaves you 2 blocks due South of your starting position, which is 2 blocks away.
 R5, L5, R5, R3 leaves you 12 blocks away.
 How many blocks away is Easter Bunny HQ?
-"""
 
-INPUT = """
-R4, R1, L2, R1, L1, L1, R1, L5, R1, R5, L2, R3, L3, L4, R4, R4, R3, L5, L1, R5, R3, L4, R1, R5, L1, R3, L2, R3, R1, L4, L1, R1, L1, L5, R1, L2, R2, L3, L5, R1, R5, L1, R188, L3, R2, R52, R5, L3, R79, L1, R5, R186, R2, R1, L3, L5, L2, R2, R4, R5, R5, L5, L4, R5, R3, L4, R4, L4, L4, R5, L4, L3, L1, L4, R1, R2, L5, R3, L4, R3, L3, L5, R1, R1, L3, R2, R1, R2, R2, L4, R5, R1, R3, R2, L2, L2, L1, R2, L1, L3, R5, R1, R4, R5, R2, R2, R4, R4, R1, L3, R4, L2, R2, R1, R3, L5, R5, R2, R5, L1, R2, R4, L1, R5, L3, L3, R1, L4, R2, L2, R1, L1, R4, R3, L2, L3, R3, L2, R1, L4, R5, L1, R5, L2, L1, L5, L2, L5, L2, L4, L2, R3
+--- Part Two ---
+
+Then, you notice the instructions continue on the back of the Recruiting Document. Easter Bunny HQ is actually at the first location you visit twice.
+
+For example, if your instructions are R8, R4, R4, R8, the first location you visit twice is 4 blocks away, due East.
+
+How many blocks away is the first location you visit twice?
 """
+from __future__ import unicode_literals, division
+
+TEXT_INPUT = 'day1_input.txt'
 
 
 # Transformation table to give appropriate new x, y values depending on facing.
@@ -40,15 +47,15 @@ TURNS = {
 }
 
 
-def parse_input(input_):
+def parse_input(filename):
     """Parse a long input string of instructions."""
-    for line in input_.strip().split('\n'):
-        for instruction in line.split(', '):
-            print(instruction)
-            yield instruction[0], int(instruction[1:])
+    with open(filename, 'r') as text_file:
+        for line in text_file:
+            for instruction in line.strip().split(', '):
+                yield instruction[0], int(instruction[1:])
 
 
-def calc_blocks_away(instructions):
+def calc_final_blocks_away(instructions):
     """Calculate the distance in blocks of the final position."""
     x, y, facing = 0, 0, 'N'
     for turn, distance in instructions:
@@ -57,7 +64,25 @@ def calc_blocks_away(instructions):
     return abs(x) + abs(y)
 
 
+def calc_first_revisited_blocks_away(instructions):
+    """Calculate the distance in blocks of the first re-visited position."""
+    visited = set()
+    x, y, facing = 0, 0, 'N'
+    for turn, distance in instructions:
+        facing = TURNS[turn][facing]
+        for _ in range(distance):
+            x, y = MOVES[facing](x, y, 1)
+            if (x, y) in visited:
+                return abs(x) + abs(y)
+            visited.add((x, y))
+    return abs(x) + abs(y)
+
+
 if __name__ == '__main__':
-    instructions = parse_input(INPUT)
-    result = calc_blocks_away(instructions)
-    print('The Easter Bunny is {} blocks away.'.format(result))
+    instructions = parse_input(TEXT_INPUT)
+    result1 = calc_final_blocks_away(instructions)
+    print('The final destination is {} blocks away.'.format(result1))
+
+    instructions = parse_input(TEXT_INPUT)
+    result2 = calc_first_revisited_blocks_away(instructions)
+    print('The first revisted location is {} blocks away.'.format(result2))
