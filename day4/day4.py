4c1d789b63b76bb2f6525b65a9235b08bce2cfa5
Will Weatherford <weatherford.william@gmail.com>
2016-12-03 23:05:02 -0800

    Implemented rotate name test for part 2

--- day4/day4.py
+++ day4/day4.py
@@ -15,6 +15,18 @@ A room is real (not a decoy) if the checksum is the five most common letters in 
 Of the real rooms from the list above, the sum of their sector IDs is 1514.
 
 What is the sum of the sector IDs of the real rooms?
+
+--- Part Two ---
+
+With all the decoy data out of the way, it's time to decrypt this list and get moving.
+
+The room names are encrypted by a state-of-the-art shift cipher, which is nearly unbreakable without the right software. However, the information kiosk designers at Easter Bunny HQ were not expecting to deal with a master cryptographer like yourself.
+
+To decrypt a room name, rotate each letter forward through the alphabet a number of times equal to the room's sector ID. A becomes B, B becomes C, Z becomes A, and so on. Dashes become spaces.
+
+For example, the real name for qzmt-zixmtkozy-ivhz-343 is very encrypted name.
+
+What is the sector ID of the room where North Pole objects are stored?
 """
 
 from __future__ import unicode_literals, division
@@ -64,7 +76,15 @@ def order_name(name):
     return ''.join(map(itemgetter(0), by_count))[:5]
 
 
+def rotate_name(name, sector):
+    """Rotate name forward in alphabet number of spaces equal to its sector."""
+    
+
+
 if __name__ == '__main__':
     rooms = input_lines('input.txt')
     result1 = sum_real_sectors(rooms)
     print('Sum of all sector IDs is {}'.format(result1))
+    rooms = input_lines('input.txt')
+    # result1 = sum_real_sectors(rooms)
+    # print('Sum of all sector IDs is {}'.format(result1))
