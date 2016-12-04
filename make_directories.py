bc5936a5f65813fc535e90b9a599f2235d90e0d3
Will Weatherford <weatherford.william@gmail.com>
2016-12-04 01:35:08 -0800

    Added script to automate setup.

---
+++ make_directories.py
@@ -0,0 +1,28 @@
+"""Setup for Advent of Code by making git branches and directories."""
+from __future__ import unicode_literals, division
+from subprocess import call
+import os
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+
+def test_names():
+    """."""
+    for n in range(2):
+        yield 'test{}'.format(n)
+
+
+def main(names):
+    """."""
+    for name in names:
+        call(['git', 'checkout', '-b', name])
+        path = os.path.join(HERE, name)
+        os.mkdir(path)
+        call(['touch', '.'.join((name, 'txt'))], cwd=path)
+        call(['git', 'add', '.'.join((name, 'txt'))], cwd=path)
+        call(['git', 'checkout', 'master'])
+
+
+if __name__ == '__main__':
+    names = test_names()
+    main(names)
