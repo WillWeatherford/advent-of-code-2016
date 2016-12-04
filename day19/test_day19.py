23d7403b444862f4db534f04be2bbca1e440e8af
Will Weatherford <weatherford.william@gmail.com>
2016-12-04 15:32:02 -0800

    Committing day19 files.

---
+++ day19/test_day19.py
@@ -0,0 +1,17 @@
+"""Tests for Advent of Code solution."""
+from __future__ import unicode_literals, division
+import pytest
+
+CASES = [
+    (),
+]
+
+
+@pytest.mark.parametrize('', CASES)
+def test_part1():
+    """Test that part1 function returns expected result."""
+
+
+@pytest.mark.parametrize('', CASES)
+def test_part2():
+    """Test that part1 function returns expected result."""
