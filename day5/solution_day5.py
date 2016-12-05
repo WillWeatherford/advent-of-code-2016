00651aa5633a87c2a43ee900237a5d977066b46e
Will Weatherford <weatherford.william@gmail.com>
2016-12-05 11:02:37 -0800

    Working iterative algorithm. How to make faster?

--- day5/solution_day5.py
+++ day5/solution_day5.py
@@ -1,7 +1,27 @@
 """
+--- Day 5: How About a Nice Game of Chess? ---
 
+You are faced with a security door designed by Easter Bunny engineers that seem to have acquired most of their security knowledge by watching hacking movies.
+
+The eight-character password for the door is generated one character at a time by finding the MD5 hash of some Door ID (your puzzle input) and an increasing integer index (starting with 0).
+
+A hash indicates the next character in the password if its hexadecimal representation starts with five zeroes. If it does, the sixth character in the hash is the next character of the password.
+
+For example, if the Door ID is abc:
+
+    The first index which produces a hash that starts with five zeroes is 3231929, which we find by hashing abc3231929; the sixth character of the hash, and thus the first character of the password, is 1.
+    5017308 produces the next interesting hash, which starts with 000008f82..., so the second character of the password is 8.
+    The third time a hash starts with five zeroes is for abc5278568, discovering the character f.
+
+In this example, after continuing this search a total of eight times, the password is 18f47a30.
+
+Given the actual Door ID, what is the password?
 """
 from __future__ import unicode_literals, division
+from itertools import count
+from hashlib import md5
+
+FIVE_ZEROES = '0' * 5
 
 
 def part1(lines):
@@ -10,3 +30,26 @@ def part1(lines):
 
 def part2(lines):
     """Run solution for Part 2."""
+
+
+def get_password_char(bytes_val):
+    """Return a single string character; the 6th char in a md5 hash of val.
+
+    If the hashed does not start with five zeroes, returns empty string.
+    """
+    hashed = md5(bytes_val)
+    hexed = hashed.hexdigest()
+    if hexed.startswith(FIVE_ZEROES):
+        return hexed[5]
+    return ''
+
+
+def decode_password(door_id):
+    """Return decoded password."""
+    result = ''
+    for n in count():
+        bytes_val = '{}{}'.format(door_id, n).encode('ascii')
+        result += get_password_char(bytes_val)
+        if len(result) >= 8:
+            break
+    return result
--- day5/test_day5.py
+++ day5/test_day5.py
@@ -3,15 +3,25 @@ from __future__ import unicode_literals, division
 import pytest
 
 CASES = [
-    (),
+    ('abc', '18f47a30'),
 ]
 
 
-@pytest.mark.parametrize('', CASES)
-def test_part1():
-    """Test that part1 function returns expected result."""
+SINGLE_CHAR_CASES = [
+    (b'abc3231929', '1'),
+]
 
 
-@pytest.mark.parametrize('', CASES)
-def test_part2():
-    """Test that part1 function returns expected result."""
+@pytest.mark.parametrize('door_id, result', CASES)
+def test_decode_password(door_id, result):
+    """Test that decode_password function returns expected result."""
+    from .solution_day5 import decode_password
+    assert decode_password(door_id) == result
+
+
+@pytest.mark.parametrize('bytes_val, result', SINGLE_CHAR_CASES)
+def test_get_password_char(bytes_val, result):
+    """Test that get_password_char function returns expected char."""
+    from .solution_day5 import get_password_char
+    assert get_password_char(bytes_val) == result
+
