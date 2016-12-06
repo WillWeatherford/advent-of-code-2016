57ce79df69d9fe6d46ee42dbe491232306265d6b
Will Weatherford <weatherford.william@gmail.com>
2016-12-05 23:37:42 -0800

    Finished refactoring of main loop.

--- day6/solution_day6.py
+++ day6/solution_day6.py
@@ -48,23 +48,14 @@ INPUT_LEN = 8
 
 def part1(lines):
     """Run solution for Part 1."""
-    table = {n: Counter() for n in range(INPUT_LEN)}
-    for line in lines:
-        for idx, char in enumerate(line):
-            table[idx].update(char)
-
-    result = [table[idx].most_common()[0][0] for idx in range(INPUT_LEN)]
-    print(''.join(result))
+    result = most_common_per_index(lines)
+    print('The decoded message is: {}'.format(result))
 
 
 def part2(lines):
     """Run solution for Part 2."""
-    table = {n: Counter() for n in range(INPUT_LEN)}
-    for line in lines:
-        for idx, char in enumerate(line):
-            table[idx].update(char)
-    result = [next(reversed(table[idx].most_common()))[0] for idx in range(INPUT_LEN)]
-    print(''.join(result))
+    result = most_common_per_index(lines, reverse=True)
+    print('The modified decoded message is: {}'.format(result))
 
 
 def most_common_per_index(lines, reverse=False):
