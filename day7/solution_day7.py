48d165ebcdaa6af758de8161518b29ff36153c10
Will Weatherford <weatherford.william@gmail.com>
2016-12-07 00:48:24 -0800

    Better refactor for code organization and speed; doesn't give right result.

--- day7/solution_day7.py
+++ day7/solution_day7.py
@@ -5,70 +5,51 @@ from __future__ import unicode_literals, division
 
 
 def part1(lines):
-    """Run solution for Part 1."""
     count = 0
     for line in lines:
-        outsides, insides = parse_parts(line)
-        for part in insides:
-            if has_abba(part):
-                break
-        else:
-            for part in outsides:
-                if has_abba(part):
-                    count += 1
-                    break
-    print(count)
-
-
-def parse_parts(line):
-    outsides = set()
-    insides = set()
-    while True:
-        try:
-            outside, line = line.split('[', 1)
-        except ValueError:
-            outsides.add(line)
-            break
-        inside, line = line.split(']', 1)
-        outsides.add(outside)
-        insides.add(inside)
-    return outsides, insides
-
-
-def has_abba(part):
-    for idx, char in enumerate(part[:-3]):
-        if char == part[idx + 3] and part[idx + 1] == part[idx + 2] and char != part[idx + 1]:
-            return True
-    return False
-
-
-def get_abas(part):
-    for idx, char in enumerate(part[:-2]):
-        if char == part[idx + 2] and char != part[idx + 1]:
-            yield part[idx:idx + 3]
-
-
-def has_bab(part, a, b):
-    for idx, char in enumerate(part[:-2]):
-        if char == b and part[idx + 2] == b and part[idx + 1] == a:
-            return True
-    return False
+        outers, inners = collect_matches(line, abba)
+        count += bool(outers) and not bool(inners)
+    print('{} IP addresses support Transport Layer Snooping.'.format(count))
 
 
 def part2(lines):
     """Run solution for Part 2."""
     count = 0
     for line in lines:
-        outsides, insides = parse_parts(line)
-        abas = set()
-        for part in outsides:
-            abas.update(get_abas(part))
-        for aba in abas:
-            for part in insides:
-                if has_bab(part, *aba[:2]):
-                    count += 1
-                    break
-            else:
-                continue
+        outers, inners = collect_matches(line, aba)
+        for found in outers:
+            if invert(found) in inners:
+                count += 1
+                break
+    print('{} IP addresses support Super-Secret Listening'.format(count))
+
+
+def collect_matches(line, match_check):
+    outers = set()
+    inners = set()
+    location = outers
+    for idx in range(len(line)):
+        if idx >= len(line) - 4:
             break
-    print(count)
+        if line[idx] == '[':
+            location = inners
+        if line[idx] == ']':
+            location = outers
+        found = match_check(idx, line)
+        if found:
+            location.add(found)
+    return outers, inners
+
+
+def abba(idx, chars):
+    a, b, c, d = chars[idx:idx + 4]
+    return all((a == d, a != b, b == c)) and chars[idx:idx + 4]
+
+
+def aba(idx, chars):
+    a, b, c = chars[idx:idx + 3]
+    return all((a == c, a != b)) and chars[idx:idx + 3]
+
+
+def invert(aba):
+    return aba[1] + aba[:2]
