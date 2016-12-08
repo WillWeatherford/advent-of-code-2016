3dcef9c1209b69d803ef48f7ef9d12453165f3c8
Will Weatherford <weatherford.william@gmail.com>
2016-12-07 22:21:33 -0800

    OK working class-based solution.

--- day8/solution_day8.py
+++ day8/solution_day8.py
@@ -1,12 +1,122 @@
 """
+--- Day 8: Two-Factor Authentication ---
+
+You come across a door implementing what you can only assume is an implementation of two-factor authentication after a long game of requirements telephone.
+
+To get past the door, you first swipe a keycard (no problem; there was one on a nearby desk). Then, it displays a code on a little screen, and you type that code on a keypad. Then, presumably, the door unlocks.
+
+Unfortunately, the screen has been smashed. After a few minutes, you've taken everything apart and figured out how it works. Now you just have to work out what the screen would have displayed.
+
+The magnetic strip on the card you swiped encodes a series of instructions for the screen; these instructions are your puzzle input. The screen is 50 pixels wide and 6 pixels tall, all of which start off, and is capable of three somewhat peculiar operations:
+
+    rect AxB turns on all of the pixels in a rectangle at the top-left of the screen which is A wide and B tall.
+    rotate row y=A by B shifts all of the pixels in row A (0 is the top row) right by B pixels. Pixels that would fall off the right end appear at the left end of the row.
+    rotate column x=A by B shifts all of the pixels in column A (0 is the left column) down by B pixels. Pixels that would fall off the bottom appear at the top of the column.
+
+For example, here is a simple sequence on a smaller screen:
+
+    rect 3x2 creates a small rectangle in the top-left corner:
+
+    ###....
+    ###....
+    .......
+
+    rotate column x=1 by 1 rotates the second column down by one pixel:
+
+    #.#....
+    ###....
+    .#.....
+
+    rotate row y=0 by 4 rotates the top row right by four pixels:
+
+    ....#.#
+    ###....
+    .#.....
+
+    rotate row x=1 by 1 again rotates the second column down by one pixel, causing the bottom pixel to wrap back to the top:
+
+    .#..#.#
+    #.#....
+    .#.....
+
+As you can see, this display technology is extremely powerful, and will soon dominate the tiny-code-displaying-screen market. That's what the advertisement on the back of the display tries to convince you, anyway.
+
+There seems to be an intermediate check of the voltage used by the display: after you swipe your card, if the screen did work, how many pixels should be lit?
 
 """
 from __future__ import unicode_literals, division
-
+from itertools import chain
 
 def part1(lines):
     """Run solution for Part 1."""
+    display = TinyDisplay()
+    for line in lines:
+        display.parse_command(line)
+    print('{} cells are lit.'.format(display.count_lit()))
 
 
 def part2(lines):
     """Run solution for Part 2."""
+    display = TinyDisplay()
+    for line in lines:
+        display.parse_command(line)
+    display.print_code()
+
+
+class TinyDisplay(object):
+
+    def __init__(self):
+        self.height = 6
+        self.width = 50
+        self.grid = [[0] * self.width for _ in range(self.height)]
+
+    def parse_command(self, line):
+        """Parse a text command."""
+        # r'(?P<cmd>rect|rotate\srow|rotate\scolumn)\s()'
+        if line.startswith('rect'):
+            cmd, rest = line.split()
+            args = rest.split('x')
+        else:
+            parts = line.split()
+            cmd = '_'.join(parts[:2])
+            _, pos = parts[2].split('=')
+            args = (pos, parts[-1])
+        method = getattr(self, cmd)
+        method(*map(int, args))
+
+    def rect(self, cols, rows):
+        """turns on all of the pixels in a rectangle at the top-left of the
+        screen which is A wide and B tall.
+        """
+        for y in range(rows):
+            for x in range(cols):
+                self.grid[y][x] = 1
+
+    def rotate_row(self, row, shifts):
+        """Shifts all of the pixels in row A (0 is the top row) right by B
+        pixels. Pixels that would fall off the right end appear at the left end of the row.
+        """
+        new_row = []
+        for x in range(self.width):
+            new_row.append(self.grid[row][x - shifts])
+        self.grid[row] = new_row
+
+    def rotate_column(self, col, shifts):
+        """shifts all of the pixels in column A (0 is the left column) down by B pixels.
+        Pixels that would fall off the bottom appear at the top of the column.
+        """
+        current_col = [row[col] for row in self.grid]
+        for y in range(self.height):
+            self.grid[y][col] = current_col[y - shifts]
+
+    def count_lit(self):
+        """Return number of lit cells."""
+        return sum(chain(*self.grid))
+
+    def print_code(self):
+        for topleft in range(0, self.width, 5):
+            segments = [row[topleft:topleft + 5] for row in self.grid]
+            segments = [''.join(map(lambda c: '#' if c else ' ', seg))
+                        for seg in segments]
+            print('\n'.join(segments))
+            print('\n')
--- day8/test_day8.py
+++ day8/test_day8.py
@@ -2,16 +2,105 @@
 from __future__ import unicode_literals, division
 import pytest
 
-CASES = [
-    (),
+RECT_CASES = [
+    (
+        [[0, 0, 0],
+         [0, 0, 0],
+         [0, 0, 0]],
+        1, 1,
+        [[1, 0, 0],
+         [0, 0, 0],
+         [0, 0, 0]],
+    ),
+    (
+        [[0, 0, 0],
+         [0, 0, 0],
+         [0, 0, 0]],
+        2, 2,
+        [[1, 1, 0],
+         [1, 1, 0],
+         [0, 0, 0]],
+    ),
 ]
 
 
-@pytest.mark.parametrize('', CASES)
-def test_part1():
-    """Test that part1 function returns expected result."""
+ROTATE_ROW_CASES = [
+    (
+        [[1, 1, 0],
+         [1, 1, 0],
+         [0, 0, 0]],
+        1, 1,
+        [[1, 1, 0],
+         [0, 1, 1],
+         [0, 0, 0]],
+    ),
+    (
+        [[1, 1, 0],
+         [1, 1, 0],
+         [0, 0, 0]],
+        1, 2,
+        [[1, 1, 0],
+         [1, 0, 1],
+         [0, 0, 0]],
+    ),
+]
 
 
-@pytest.mark.parametrize('', CASES)
-def test_part2():
+ROTATE_COL_CASES = [
+    (
+        [[1, 1, 0],
+         [1, 1, 0],
+         [0, 0, 0]],
+        1, 1,
+        [[1, 0, 0],
+         [1, 1, 0],
+         [0, 1, 0]],
+    ),
+    (
+        [[1, 1, 0],
+         [1, 1, 0],
+         [0, 0, 0]],
+        1, 2,
+        [[1, 1, 0],
+         [1, 0, 0],
+         [0, 1, 0]],
+    ),
+]
+
+
+@pytest.mark.parametrize('start, cols, rows, result', RECT_CASES)
+def test_rect(start, cols, rows, result):
     """Test that part1 function returns expected result."""
+    from .solution_day8 import TinyDisplay
+    dis = TinyDisplay()
+    dis.grid = start
+    dis.rect(cols, rows)
+    assert dis.grid == result
+
+
+@pytest.mark.parametrize('start, row, shifts, result', ROTATE_ROW_CASES)
+def test_rotate_row(start, row, shifts, result):
+    """Test that part1 function returns expected result."""
+    from .solution_day8 import TinyDisplay
+    dis = TinyDisplay()
+    dis.height = len(start)
+    dis.width = len(start[0])
+    dis.grid = start
+    dis.rotate_row(row, shifts)
+    assert dis.grid == result
+
+
+@pytest.mark.parametrize('start, col, shifts, result', ROTATE_COL_CASES)
+def test_rotate_col(start, col, shifts, result):
+    """Test that part1 function returns expected result."""
+    from .solution_day8 import TinyDisplay
+    dis = TinyDisplay()
+    dis.height = len(start)
+    dis.width = len(start[0])
+    dis.grid = start
+    dis.rotate_column(col, shifts)
+    assert dis.grid == result
+
+# @pytest.mark.parametrize('', )
+# def test_part2():
+#     """Test that part1 function returns expected result."""
