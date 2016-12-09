443fa6cf672e121036d602f03b1bd043ce528d49
Will Weatherford <weatherford.william@gmail.com>
2016-12-09 00:18:12 -0800

    Passing part1, seems like part2 should pass but it goes on forever.

--- day9/solution_day9.py
+++ day9/solution_day9.py
@@ -2,11 +2,42 @@
 
 """
 from __future__ import unicode_literals, division
+import re
+
+PAT = r'\((?P<num>\d+)x(?P<mult>\d+)\)'
 
 
 def part1(lines):
     """Run solution for Part 1."""
+    line = next(lines)
+    result = sum(decompress(line, False))
+    print(result)
 
 
 def part2(lines):
     """Run solution for Part 2."""
+    line = next(lines)
+    result = sum(decompress(line, True))
+    print(result)
+
+
+def decompress(chars, part2):
+    while chars:
+        match = re.search(PAT, chars)
+        if match is None:
+            yield len(chars)
+            break
+        else:
+            before = chars[:match.start()]
+            yield len(before)
+            chars = chars[match.end():]
+            num, mult = map(int, match.groups())
+            to_multipy = chars[:num]
+            chars = chars[num:]  # remainder
+
+            if not part2:
+                yield num * mult
+                continue
+
+            for count in decompress(to_multipy * mult, part2):
+                yield count
--- day9/test_day9.py
+++ day9/test_day9.py
@@ -2,16 +2,25 @@
 from __future__ import unicode_literals, division
 import pytest
 
-CASES = [
-    (),
+PART2_CASES = [
+    ('(3x3)XYZ', len('XYZXYZXYZ')),
+    ('X(8x2)(3x3)ABCY', len('XABCABCABCABCABCABCY')),
+    ('(27x12)(20x12)(13x14)(7x10)(1x12)A', 241920),
+    ('(25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN', 445),
 ]
 
 
-@pytest.mark.parametrize('', CASES)
-def test_part1():
-    """Test that part1 function returns expected result."""
+# @pytest.mark.parametrize('', CASES)
+# def test_part1():
+#     """Test that part1 function returns expected result."""
 
 
-@pytest.mark.parametrize('', CASES)
-def test_part2():
+@pytest.mark.parametrize('string, result', PART2_CASES)
+def test_part2(string, result):
     """Test that part1 function returns expected result."""
+    from .solution_day9 import decompress
+    assert sum(decompress(string, True)) == result
+
+
+def test_specific():
+    pass
