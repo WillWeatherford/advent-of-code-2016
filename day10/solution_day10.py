a20f675056f964d4e831fd563ef83645b90613cc
Will Weatherford <weatherford.william@gmail.com>
2016-12-09 23:38:34 -0800

    Changed dict names to pluralized

--- day10/solution_day10.py
+++ day10/solution_day10.py
@@ -11,29 +11,29 @@ GIVE_PAT = r'^bot (\d+) gives low to (bot|output) (\d+) and high to (bot|output)
 
 def part1(lines):
     """Run solution for Part 1."""
-    bot = {}
-    output = {}
+    bots = {}
+    outputs = {}
     to_do = deque(lines)
     while to_do:
         line = to_do.pop()
         if line.startswith('value'):
             chip, getter = re.match(IN_PAT, line).groups()
-            bot.setdefault(getter, []).append(int(chip))
+            bots.setdefault(getter, []).append(int(chip))
         elif line.startswith('bot'):
             giver, low_type, low_dest, high_type, high_dest = re.match(GIVE_PAT, line).groups()
-            if len(bot.get(giver, [])) < 2:
+            if len(bots.get(giver, [])) < 2:
                 to_do.appendleft(line)
                 continue
-            low, high = sorted(bot[giver])
+            low, high = sorted(bots[giver])
             if low == 17 and high == 61:
                 print('TARGET FOUND')
                 print(giver)
-            locals()[low_type].setdefault(low_dest, []).append(low)
-            locals()[high_type].setdefault(high_dest, []).append(high)
-            bot[giver] = []
+            locals()[low_type + 's'].setdefault(low_dest, []).append(low)
+            locals()[high_type + 's'].setdefault(high_dest, []).append(high)
+            bots[giver] = []
     prod = 1
     for n in range(3):
-        prod *= output[str(n)].pop()
+        prod *= outputs[str(n)].pop()
     print(prod)
 
 
