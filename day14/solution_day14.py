6f59be51bba485ac287e5141a354b872d77b2392
Will Weatherford <weatherford.william@gmail.com>
2016-12-13 22:07:39 -0800

    Not working original solution; trying something different.

--- day14/solution_day14.py
+++ day14/solution_day14.py
@@ -10,27 +10,51 @@ def part1(lines):
     """Run solution for Part 1."""
     found = 0
     salt = 'ihaygndm'
-    candidates = {}
+    candidates = []
     for n in count():
         bytes_val = get_bytes_val(salt, n)
         hashed_hex = get_hashed_hex(bytes_val)
-        triplet = get_triple(hashed_hex)
-        if triplet:
-            candidates[triplet] = 1000
 
-        quint = get_quint(hashed_hex)
-        if quint and quint[:3] in candidates:
-            found += 1
+        quint = get_repeat(hashed_hex, 5)
+        if quint:
+            match_index = match_quint(quint, candidates)
+            if match_index is not None:
+                found += 1
+                if found == 64:
+                    print(match_index)
+                    break
+        else:
+            triplet = get_repeat(hashed_hex, 3)
+            if triplet:
+                candidates.append((triplet, 1000, n))
 
         candidates = update_candidates(candidates)
 
 
+def match_quint(quint, candidates):
+    match_index = None
+    candidates_copy = []
+    for triplet, coundown, n in candidates:
+        if match_index is None and triplet == quint[:3]:
+            match_index = n
+        else:
+            candidates_copy.append((triplet, coundown, n))
+    return match_index
+
+
+def get_repeat(chars, num):
+    for idx, char in enumerate(chars):
+        if chars[idx:idx + num] == char * num:
+            return chars[idx:idx + num]
+    return ''
+
+
 def update_candidates(candidates):
-    candidates_copy = {}
-    for cand, countdown in candidates.items():
+    candidates_copy = []
+    for triplet, countdown, n in candidates:
         new_count = countdown - 1
         if new_count > 0:
-            candidates_copy[cand] = new_count
+            candidates_copy.append((triplet, new_count, n))
     return candidates_copy
 
 
