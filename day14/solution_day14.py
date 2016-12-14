07b60fe5b6d0978f870e4a2daf9e8e239e7f0602
Will Weatherford <weatherford.william@gmail.com>
2016-12-13 23:02:25 -0800

    Working solution; using deque of a rolling 1000 hashes, to minimize re-stretching.

--- day14/solution_day14.py
+++ day14/solution_day14.py
@@ -1,45 +1,48 @@
 """
+Inspired by this solution: https://github.com/bpeel/advent2016/blob/master/day14.py
 
 """
 from __future__ import unicode_literals, division
 from itertools import count
 from hashlib import md5
+from collections import deque
 
 
 def part1(lines):
     """Run solution for Part 1."""
+    salt = next(lines)
+    n = 64
+    result = find_nth_key_index(salt, 64)
+    print('{}th key found at the {} index.'.format(n, result))
+
+
+def part2(lines):
+    """Run solution for Part 2."""
+    salt = next(lines)
+    n = 64
+    result = find_nth_key_index(salt, 64, stretch=True)
+    print('{}th key found at the {} index.'.format(n, result))
+
+
+def find_nth_key_index(salt, n, stretch=False):
     found = 0
-    salt = 'ihaygndm'
-    candidates = []
-    for n in count():
-        bytes_val = get_bytes_val(salt, n)
-        hashed_hex = get_hashed_hex(bytes_val)
+    hashes = deque(get_hashed_hex(salt, idx, stretch) for idx in range(1001))
 
-        quint = get_repeat(hashed_hex, 5)
-        if quint:
-            match_index = match_quint(quint, candidates)
-            if match_index is not None:
-                found += 1
-                if found == 64:
-                    print(match_index)
+    for idx in count():
+        current = hashes.popleft()
+
+        triplet = get_repeat(current, 3)
+        if triplet:
+            for other_hash in hashes:
+                quint = get_repeat(other_hash, 5)
+                if triplet == quint[:3]:
+                    found += 1
                     break
-        else:
-            triplet = get_repeat(hashed_hex, 3)
-            if triplet:
-                candidates.append((triplet, 1000, n))
+        if found == 64:
+            break
+        hashes.append(get_hashed_hex(salt, idx + 1001, stretch))
 
-        candidates = update_candidates(candidates)
-
-
-def match_quint(quint, candidates):
-    match_index = None
-    candidates_copy = []
-    for triplet, coundown, n in candidates:
-        if match_index is None and triplet == quint[:3]:
-            match_index = n
-        else:
-            candidates_copy.append((triplet, coundown, n))
-    return match_index
+    return idx
 
 
 def get_repeat(chars, num):
@@ -49,29 +52,11 @@ def get_repeat(chars, num):
     return ''
 
 
-def update_candidates(candidates):
-    candidates_copy = []
-    for triplet, countdown, n in candidates:
-        new_count = countdown - 1
-        if new_count > 0:
-            candidates_copy.append((triplet, new_count, n))
-    return candidates_copy
-
-
-def part2(lines):
-    """Run solution for Part 2."""
-
-
-def get_bytes_val(door_id, n):
-    """Return a bytestring concatenation of door_id and index n."""
-    return '{}{}'.format(door_id, n).encode('ascii')
-
-
-def get_hashed_hex(bytes_val):
-    """Return a single string character; the 6th char in a md5 hash of val.
-
-    If the hashed does not start with five zeroes, returns empty string.
-    """
-    hashed = md5(bytes_val)
-    return hashed.hexdigest()
-
+def get_hashed_hex(salt, idx, stretch):
+    """."""
+    bytes_val = '{}{}'.format(salt, idx).encode('ascii')
+    hashed = md5(bytes_val).hexdigest()
+    if stretch:
+        for _ in range(2016):
+            hashed = md5(hashed.encode('ascii')).hexdigest()
+    return hashed
