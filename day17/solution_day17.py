f6d7f9145c385612c48fb2942417c611b3426220
Will Weatherford <weatherford.william@gmail.com>
2016-12-16 23:46:23 -0800

    Refactored to both use the same algorithm finding ALL paths.

--- day17/solution_day17.py
+++ day17/solution_day17.py
@@ -1,4 +1,50 @@
 """
+--- Day 17: Two Steps Forward ---
+
+You're trying to access a secure vault protected by a 4x4 grid of small rooms connected by doors. You start in the top-left room (marked S), and you can access the vault (marked V) once you reach the bottom-right room:
+
+#########
+#S| | | #
+#-#-#-#-#
+# | | | #
+#-#-#-#-#
+# | | | #
+#-#-#-#-#
+# | | |  
+####### V
+
+Fixed walls are marked with #, and doors are marked with - or |.
+
+The doors in your current room are either open or closed (and locked) based on the hexadecimal MD5 hash of a passcode (your puzzle input) followed by a sequence of uppercase characters representing the path you have taken so far (U for up, D for down, L for left, and R for right).
+
+Only the first four characters of the hash are used; they represent, respectively, the doors up, down, left, and right from your current position. Any b, c, d, e, or f means that the corresponding door is open; any other character (any number or a) means that the corresponding door is closed and locked.
+
+To access the vault, all you need to do is reach the bottom-right room; reaching this room opens the vault and all doors in the maze.
+
+For example, suppose the passcode is hijkl. Initially, you have taken no steps, and so your path is empty: you simply find the MD5 hash of hijkl alone. The first four characters of this hash are ced9, which indicate that up is open (c), down is open (e), left is open (d), and right is closed and locked (9). Because you start in the top-left corner, there are no "up" or "left" doors to be open, so your only choice is down.
+
+Next, having gone only one step (down, or D), you find the hash of hijklD. This produces f2bc, which indicates that you can go back up, left (but that's a wall), or right. Going right means hashing hijklDR to get 5745 - all doors closed and locked. However, going up instead is worthwhile: even though it returns you to the room you started in, your path would then be DU, opening a different set of doors.
+
+After going DU (and then hashing hijklDU to get 528e), only the right door is open; after going DUR, all doors lock. (Fortunately, your actual passcode is not hijkl).
+
+Passcodes actually used by Easter Bunny Vault Security do allow access to the vault if you know the right path. For example:
+
+    If your passcode were ihgpwlah, the shortest path would be DDRRRD.
+    With kglvqrro, the shortest path would be DDUDRLRRUDRD.
+    With ulqzkmiv, the shortest would be DRURDRUDDLLDLUURRDULRLDUUDDDRR.
+
+Given your vault's passcode, what is the shortest path (the actual path, not just the length) to reach the vault?
+
+Your puzzle answer was DRRDRLDURD.
+--- Part Two ---
+
+You're curious how robust this security solution really is, and so you decide to find longer and longer paths which still provide access to the vault. You remember that paths always end the first time they reach the bottom-right room (that is, they can never pass through it, only end in it).
+
+For example:
+
+    If your passcode were ihgpwlah, the longest path would take 370 steps.
+    With kglvqrro, the longest path would be 492 steps long.
+    With ulqzkmiv, the longest path would be 830 steps long.
 
 """
 from __future__ import unicode_literals, division
@@ -15,61 +61,52 @@ DIRECTIONS = [
 ]
 
 SALT = 'pvhmgsws'
+START = (0, 0, '')
+END = (3, 3)
+
 
 def part1(lines):
     """Run solution for Part 1."""
-    result = solve((0, 0, ''), (3, 3))
-    print(result)
+    paths = find_all_paths(START, END)
+    result = min(paths, key=len)
+    print('Shortest possible path from start to vault:\n{}'.format(result))
 
 
 def part2(lines):
     """Run solution for Part 2."""
+    paths = find_all_paths(START, END)
+    result = len(max(paths, key=len))
+    print('Longest possible path from start to vault:\n{}'.format(result))
 
 
-def solve(start, exit):
+def find_all_paths(start, exit):
     found_paths = set()
     to_do = deque([start])
 
     while to_do:
         x, y, path_so_far = to_do.pop()
 
-        print('Examinging at {} {}'.format(x, y))
-
         if (x, y) == exit:
             found_paths.add(path_so_far)
             continue
 
-        for move, n_x, n_y in get_passable_neighbors(x, y, path_so_far):
+        for move, n_x, n_y in get_open_moves(x, y, path_so_far):
             to_do.appendleft((n_x, n_y, path_so_far + move))
-            print('Found a passable neighbor: {} {}'.format(n_x, n_y))
 
-    return len(max(found_paths, key=len))
-
-
-def get_passable_neighbors(x, y, path):
-    for n_pos, door in zip(get_neighbors(x, y), get_locked_status(path)):
-        move, n_x, n_y = n_pos
-        if n_x < 0 or n_y < 0 or n_x > 3 or n_y > 3:
-            continue
-        if door:
-            yield move, n_x, n_y
+    return found_paths
 
 
-def get_neighbors(x, y):
-    for move, x_mod, y_mod in DIRECTIONS:
-        n_x = x + x_mod
-        n_y = y + y_mod
-        yield move, n_x, n_y
+def get_open_moves(x, y, path):
+    for move, door in zip(get_moves(x, y), get_door_status(path)):
+        if door and set(move[1:]).issubset(range(4)):
+            yield move
 
 
-# def make_path(found, start_pos, tile):
-#     path = []
-#     while tile != start_pos:
-#         path.append(found[tile]['move'])
-#         tile = found[tile]['prev']
-#     return path[::-1]
+def get_moves(x, y):
+    for direction, x_mod, y_mod in DIRECTIONS:
+        yield direction, x + x_mod, y + y_mod
 
 
-def get_locked_status(path):
+def get_door_status(path):
     hashed = md5((SALT + path).encode('ascii')).hexdigest()
     return map(lambda x: x in OPENS, hashed[:4])
