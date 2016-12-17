aef405e734eed4d3ca5aa71b5156c92516a99a70
Will Weatherford <weatherford.william@gmail.com>
2016-12-16 21:03:54 -0800

    Initial one func

--- day17/solution_day17.py
+++ day17/solution_day17.py
@@ -4,9 +4,15 @@
 from __future__ import unicode_literals, division
 
 
+OPENS = 'bcdef'
+
 def part1(lines):
     """Run solution for Part 1."""
 
 
 def part2(lines):
     """Run solution for Part 2."""
+
+
+def opens(hashed):
+    return map(lambda x: x in OPENS, hashed[:4])
