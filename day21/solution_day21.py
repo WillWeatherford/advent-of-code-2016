af201852ac91430e977d1024c78eaadda3771eb6
Will Weatherford <weatherford.william@gmail.com>
2016-12-22 00:11:15 -0800

    Function signatures

--- day21/solution_day21.py
+++ day21/solution_day21.py
@@ -10,3 +10,30 @@ def part1(lines):
 
 def part2(lines):
     """Run solution for Part 2."""
+
+def swap_letter(string, x, y):
+    pass
+
+
+def swap_position(string, x, y):
+    pass
+
+
+def rotate_left(string, steps):
+    pass
+
+
+def rotate_right(string, steps):
+    pass
+
+
+def rotate_based_on_index(string, letter):
+    pass
+
+
+def reverse_slice(string, start, end):
+    pass
+
+
+def move(string, from_idx, to_idx):
+    pass
