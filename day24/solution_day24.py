b254515d6995dfd51fa9b3b3fa17a61a8951ae88
Will Weatherford <weatherford.william@gmail.com>
2016-12-23 21:53:00 -0800

    Passes example.

--- day24/solution_day24.py
+++ day24/solution_day24.py
@@ -2,10 +2,99 @@
 
 """
 from __future__ import unicode_literals, division
+from heapq import heappop, heappush
+from itertools import count
 
+
+WALL = '#'
 
 def part1(lines):
     """Run solution for Part 1."""
+    num_moves = find_shortest_path(lines)
+    print(num_moves)
+
+
+def find_shortest_path(lines):
+    unique = count()
+    grid = []
+    goals = set()
+    for y, line in enumerate(lines):
+        row = []
+        for x, char in enumerate(line):
+            if char.isdigit():
+                if char == '0':
+                    start_pos = (y, x)
+                else:
+                    goals.add((y, x))
+            row.append(char)
+        grid.append(row)
+
+    # grid = [list(line) for line in lines]
+
+    # (total_distance, closest_target, num_unfound, _, pos, un_found)
+    start = (
+        0,
+        closest_target_distance(start_pos, goals),
+        len(goals),
+        next(unique),
+        start_pos,
+        goals,
+    )
+    to_do = [start]
+    while True:
+        total_distance, closest_target, num_unfound, _, pos, unfound = heappop(to_do)
+
+        # update unfound and num_unfound
+        if pos in unfound:
+            unfound = unfound - {pos}
+            num_unfound = len(unfound)
+
+        if num_unfound == 0:
+            return total_distance
+
+        for neighbor_pos in passable_neighbors(pos, grid):
+            move = (
+                total_distance + 1,
+                closest_target_distance(neighbor_pos, unfound),
+                num_unfound,
+                next(unique),
+                neighbor_pos,
+                unfound,
+            )
+            heappush(to_do, move)
+
+
+def passable_neighbors(pos, grid):
+    for y, x in find_neighbors(pos):
+        try:
+            val = grid[y][x]
+        except IndexError:
+            continue
+        else:
+            if val != WALL:
+                yield (y, x)
+
+
+def find_neighbors(pos):
+    y, x = pos
+    # up
+    if y > 0:
+        yield y - 1, x
+    # left
+    if x > 0:
+        yield y, x - 1
+    # down
+    yield y + 1, x
+    # right
+    yield y, x + 1
+
+
+
+
+def closest_target_distance(pos, unfound):
+    """Return the shortest distance to any unfound goal location from current pos."""
+    return min(abs(goal[0] - pos[0]) + abs(goal[1] - pos[1])
+               for goal in unfound)
 
 
 def part2(lines):
