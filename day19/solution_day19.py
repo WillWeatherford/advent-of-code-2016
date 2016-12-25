1cbc10ae18b57b3ea49c68c0a9eedd14a663a347
Will Weatherford <weatherford.william@gmail.com>
2016-12-25 00:31:06 -0800

    refactored part1

--- day19/solution_day19.py
+++ day19/solution_day19.py
@@ -10,18 +10,28 @@ START_ELVES = 5
 
 def part1(lines):
     """Run solution for Part 1."""
-
-
+    num_elves = int(next(lines))
+    result = steal_from_next(num_elves)
+    print('Elf #{} is left with all the presents!'.format(result))
 
 
 def part2(lines):
     """Run solution for Part 2."""
     num_elves = int(next(lines))
-    result = remove_halfway(num_elves)
+    result = steal_from_across(num_elves)
     print('Elf #{} is left with all the presents!'.format(result))
 
 
-def remove_halfway(num_elves):
+def steal_from_next(num_elves):
+    """Return the elf remaining when stealing from the next elf."""
+    elves = deque(range(1, num_elves + 1))
+    while len(elves) > 1:
+        elves.rotate(-1)
+        elves.popleft()
+    return elves[0]
+
+
+def steal_from_across(num_elves):
     """Return the elf remaining when stealing from elf across the circle."""
     elves = deque(range(1, num_elves + 1))
     elves.rotate(-((len(elves) - 1) >> 1))
